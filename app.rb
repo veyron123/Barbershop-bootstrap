37883018b5cd36c686d72e10509ad8dafa6ada05
Denis Havryliak <unitradecargo@gmail.com>
2022-06-05 16:17:59 +0300

    add colorpicker

--- app.rb
+++ app.rb
@@ -4,7 +4,37 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>	"	
+
+	
+	
+
+
+
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"	
+end
+
+post "/" do
+	@login=params[:login]
+	@password=params[:password]
+
+	if  @login == "veyron" && @password == "admin" 
+		@login_in = "Welcome to BarberShop"
+
+		erb "Welcome to site"
+		
+	else 
+		@error= "Wrong login or password"
+
+		erb "Wrong login or password"
+
+		
+		
+	end
+
+	
+	
+	
+
 end
 
 get '/about' do 
@@ -15,8 +45,17 @@ get '/contacts' do
 	erb :contacts
 end
 
-get '/visit' do 
-	erb :visit
+post '/contacts' do 
+	@email = params[:email]
+	@comment = params[:comment]
+
+	@message_contacts = "Мы ответим Вам в ближайшее время"
+
+	f = File.open "public/contacts.txt", "a"
+	f.write "#{@email} \n #{@comment}"
+	f.close
+
+	erb :contacts
 end
 
 get '/visit' do 
@@ -29,9 +68,15 @@ post '/visit' do
 	@phone = params[:phone]
 	@date = params[:date]
 	@time = params[:time]
+	@barber = params[:barber]
+
+	@message = " Дорогой #{@name} мы ждем Вас в #{@time} #{@date}, ваш парикмахер #{@barber}"
 
 	# @title = "Приветствуем Вас!"
-
+ 
+	@users= File.open "public/users.txt", "a"
+	@users.write "Имя: #{@name}, телефон - #{@phone}, время - #{@time}, дата - #{@date}, парикмахер -#{@barber}\n"
+	@users.close 
 
 	erb :visit
 end
